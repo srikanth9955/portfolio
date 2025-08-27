08f0e375435d8e9aac931b54b3bea68f292916d1
Srikanth <rayapuramsrikanth2001@gmail.com>
2025-08-27 23:13:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const typed = new Typed('.typed', {
+      strings: ["Web Developer" , "Backend Developer" , "Designer",".Net Developer"],
+      typeSpeed: 160,
+      backSpeed: 50,
+      loop: true,
+    });
+  });
